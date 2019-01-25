3410533bb23a5c1aa0ea5fa44c0cf8c4a53eb6d3
PundikUser <pundikuser@cle-fes-brainsight.v10.med.va.gov>
2019-01-25 16:37:08 -0500

    add 98% confidence intervals

--- add_point2sici.m
+++ add_point2sici.m
@@ -89,18 +89,26 @@ if height(tbl_use) >= 3
 	mean_var = [info_var '_mean' ];	
 	mean_val = mean(tbl_use.MEPAmpl_uVPp);
 	app.sici_ui.(mean_var).String = num2str(mean_val,'%6.2f');
-	sd_var = [info_var '_sd'];
+	ci_var = [info_var '_ci'];
 	sd_val = std(tbl_use.MEPAmpl_uVPp);
-	app.sici_ui.(sd_var).String = num2str(sd_val,'%6.2f');
+	ci_val = confidence_intervals(tbl_use.MEPAmpl_uVPp, 98);
+	ci_str = sprintf('[%6.2f, %6.2f]', mean_val+ci_val(1), mean_val+ci_val(2));
+	app.sici_ui.(ci_var).String = ci_str;
 	m_line_var = [info_var '_mline'];
+	
 	app.sici_ui.(m_line_var).YData = [mean_val mean_val];
 	up_line_var = [info_var '_sdupline'];
 	app.sici_ui.(up_line_var).YData = [mean_val+sd_val mean_val+sd_val];
 	dwn_line_var = [info_var '_sddownline'];
 	app.sici_ui.(dwn_line_var).YData = [mean_val-sd_val mean_val-sd_val];
+	
+	up_line_var = [info_var '_ciupline'];
+	app.sici_ui.(up_line_var).YData = [mean_val+ci_val(2) mean_val+ci_val(2)];
+	dwn_line_var = [info_var '_cidownline'];
+	app.sici_ui.(dwn_line_var).YData = [mean_val+ci_val(1) mean_val+ci_val(1)];
 
-	app.sici_info.(mean_var) = mean_val;
-	app.sici_info.(sd_var) = sd_val;
+	app.sici_info.(info_var).(mean_var) = mean_val;
+	app.sici_info.(info_var).(ci_var) = ci_val;
 
 end
 
