0c42422647022127b575ef455186a6f5a822e5ea
PundikUser <pundikuser@cle-fes-brainsight.v10.med.va.gov>
2019-02-04 14:59:31 -0500

    save printed figure in same folder as all saved files

--- print_rc.m
+++ print_rc.m
@@ -1,12 +1,25 @@
 function print_rc(source, event, app)
 
+if ~any(strcmp(properties(app), 'SaveLocationEditField')) 
+	save_loc = pwd;
+	fname_prefix = '';
+else
+	if isempty(app.SaveLocationEditField.Value)
+		app.SaveLocationEditField.Value = pwd;
+		save_loc = pwd;
+	else
+		save_loc = app.SaveLocationEditField.Value;
+	end
+	fname_prefix = app.EditFieldFilenameprefix.Value;
+end
+
 
 set(app.rc_fig,'PaperOrientation', orient, ...
 	'PaperUnits','inches', ...
 	'PaperPosition', [0 0 7 8]);
 
 % 	'PaperSize', [6 7], ..
-fname = [strrep(app.rc_axes.Title.String, ' ', '_') '_rc_not_norm.png'];
+fname = [save_loc '/' fname_prefix strrep(app.rc_axes.Title.String, ' ', '_') '_rc_not_norm.png'];
 
 % if norm value > 1, change not_norm to norm in fitinfo_fname
 if str2double(app.rc_fit_ui.edNormFactor.String) > 1
--- print_sici.m
+++ print_sici.m
@@ -1,12 +1,24 @@
 function print_rc(source, event, app)
 
+if ~any(strcmp(properties(app), 'SaveLocationEditField')) 
+	save_loc = pwd;
+	fname_prefix = '';
+else
+	if isempty(app.SaveLocationEditField.Value)
+		app.SaveLocationEditField.Value = pwd;
+		save_loc = pwd;
+	else
+		save_loc = app.SaveLocationEditField.Value;
+	end
+	fname_prefix = app.EditFieldFilenameprefix.Value;
+end
 
 set(app.sici_fig,'PaperOrientation', orient, ...
 	'PaperUnits','inches', ...
 	'PaperPosition', [0 0 7 8]);
 
 % 	'PaperSize', [6 7], ..
-fname = [strrep(app.sici_axes.Title.String, ' ', '_') '_sici.png'];
+fname = [save_loc '/' fname_prefix strrep(app.sici_axes.Title.String, ' ', '_') '_sici.png'];
 
 
 print(app.sici_fig, '-dpng', fname);
