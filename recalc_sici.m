629fe3dc50154f1d65fff99e875699e3a02cf348
PeggyS <peggy.skelly@gmail.com>
2019-05-09 11:53:19 -0400

    add button to recalc sici values after disabling/enabling datapoints in the figure

---
+++ recalc_sici.m
@@ -0,0 +1,5 @@
+function recalc_rc(source, event, app)
+
+update_sici_mean_sc_ci_lines(app, 'Test Stim', 'ts')
+update_sici_mean_sc_ci_lines(app, 'SICI', 'sici')
+update_sici_mean_sc_ci_lines(app, 'ICF', 'icf')
