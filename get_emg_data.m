88a4e4163385bf0e79561928f4e9c6b33b4481ca
PundikUser <pundikuser@cle-fes-brainsight.v10.med.va.gov>
2019-05-29 14:44:05 -0400

    add optional way computing pre stim egm value & subtracting that value from the MEP value

--- get_emg_data.m
+++ get_emg_data.m
@@ -50,7 +50,32 @@ while app.CheckBoxDisplayMEP.Value
 			% set new_data to false
 			app.emg_data_mmap.Data(1).new_data = uint8(0);
 
-
+			% monitor emg's value
+			monitor_emg_val = app.emg_data_mmap.Data(1).monitor_emg_val;
+			
+			% compute the pre-stim emg
+			left_time = app.preEmgMinEditField.Value; % 100 ms prior to stim (+ 1 ms to allow for stim artifact)
+			right_time = app.preEmgMaxEditField.Value; 
+			pre_stim_data = app.h_emg_line.YData(app.h_emg_line.XData>=left_time ...
+				& app.h_emg_line.XData<right_time);
+			if app.MeanRectifiedValueButton.Value
+				pre_stim_val = mean(abs(pre_stim_data) - mean(pre_stim_data));
+			else % using max peak to peak value
+				pre_stim_val = max(pre_stim_data) - min(pre_stim_data);
+			end
+			set(app.pre_emg_text, 'String', [num2str(monitor_emg_val) ' (' num2str(round(pre_stim_val)) ')'])
+			% change the color depending on the proximity to the goal
+% 			in_goal_range = 0;
+			if monitor_emg_val >= app.emg_data_mmap.Data(1).goal_min && ...
+					monitor_emg_val <= app.emg_data_mmap.Data(1).goal_max  % in the green
+				set(app.pre_emg_text, 'ForegroundColor', [20 224 20]/255)
+% 				in_goal_range = 1;
+			elseif monitor_emg_val < app.emg_data_mmap.Data(1).goal_min % below
+				set(app.pre_emg_text, 'ForegroundColor', [255 153 0]/255)
+			else % above
+				set(app.pre_emg_text, 'ForegroundColor', [209 36 36]/255)
+			end
+			
 			% get mep value 
 			t_emg = app.h_emg_line.XData;
 			t_mep_min = app.h_t_min_line.XData(1);
@@ -58,33 +83,19 @@ while app.CheckBoxDisplayMEP.Value
 			mep_seg = emg_data(t_emg>t_mep_min & t_emg<t_mep_max);
 
 			mep_val = max(mep_seg) - min(mep_seg);
+			
+			if app.SubtractPreEMGppButton.Value % subtract the pre stim emg
+				mep_val = mep_val - pre_stim_val;
+			end
+				
 
 			set(app.mep_value_text, 'String', num2str(round(mep_val)))
-			if mep_val >= 200
+			if mep_val >= app.MEPThresholdEditField.Value
 				set(app.mep_value_text, 'ForegroundColor', 'r')
 			else
 				set(app.mep_value_text, 'ForegroundColor', 'b')
 			end
 
-			% monitor emg's value
-			monitor_emg_val = app.emg_data_mmap.Data(1).monitor_emg_val;
-			
-			% compute the pre-stim emg
-			pre_stim_data = app.h_emg_line.YData(app.h_emg_line.XData>=-101 ...
-				& app.h_emg_line.XData<-1); % 100 ms prior to stim (+ 1 ms to allow for stim artifact)
-			pre_stim_val_100ms = mean(abs(pre_stim_data) - mean(pre_stim_data));
-			set(app.pre_emg_text, 'String', [num2str(monitor_emg_val) ' (' num2str(round(pre_stim_val_100ms)) ')'])
-			% change the color depending on the proximity to the goal
-			in_goal_range = 0;
-			if monitor_emg_val >= app.emg_data_mmap.Data(1).goal_min && ...
-					monitor_emg_val <= app.emg_data_mmap.Data(1).goal_max  % in the green
-				set(app.pre_emg_text, 'ForegroundColor', [20 224 20]/255)
-				in_goal_range = 1;
-			elseif monitor_emg_val < app.emg_data_mmap.Data(1).goal_min % below
-				set(app.pre_emg_text, 'ForegroundColor', [255 153 0]/255)
-			else % above
-				set(app.pre_emg_text, 'ForegroundColor', [209 36 36]/255)
-			end
 			drawnow
 
 			% if the rc figure window exists, plot the point
