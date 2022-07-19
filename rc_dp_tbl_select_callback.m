033073a4b5a51ee32a443a178491bc6a04fb1581
PeggyS <peggy.skelly@gmail.com>
2022-07-19 10:41:12 -0400

    Update rc_dp_tbl_select_callback.m
    Make sure it saves the autocompute mep info in the analysis folder

--- rc_dp_tbl_select_callback.m
+++ rc_dp_tbl_select_callback.m
@@ -118,11 +118,34 @@ if length(all_selected) > 1
 		end
 		% change the mep start line
 		app.h_t_min_line.XData = [mep_begin mep_begin];
-		mep_line_drag_endfcn(app.h_t_min_line)
+		mep_line_drag_endfcn(app.h_t_min_line) % FIXME - this is unselecting cells in the table
 		% save info used to compute mep_begin
 		% save in the analysis folder with datapoints.csv file
 		save_file = strrep(app.DatapointsCSVEditField.Value, 'datapoints.csv', 'mep_auto_compute_info.txt');
-		info.mep_begin_time = mep_begin;
+        % if the current directory has '/data/' in it then change it
+        % '/analysis/' to save the output there
+        [save_loc, save_f, save_ext] = fileparts(save_file);
+        if contains(save_loc, [filesep 'data' filesep], 'IgnoreCase', true)
+            save_loc = strrep(lower(save_loc), [filesep 'data' filesep], [filesep 'analysis' filesep]);
+            % ask to create the folder if it doesn't exist
+            if ~exist(save_loc, 'dir')
+                ButtonName = questdlg(['Create new directory: ' save_loc ' ?'], ...
+                    'Create new directory', ...
+                    'Yes', 'No', 'Yes');
+                if strcmp(ButtonName, 'Yes')
+                    [success, msg, msg_id] = mkdir(save_loc);
+                else
+                    disp('Choose where to save output')
+                    save_loc = uigetdir();
+                    % 				while save_loc==0
+                    % 					disp('Must choose a folder to save output.')
+                    % 					save_loc = uigetdir();
+                    % 				end
+                end
+            end
+        end
+        save_file = fullfile(save_loc, [save_f save_ext]);
+        info.mep_begin_time = mep_begin;
 		info.epochs_used = all_selected;
 		write_fit_info(save_file, info)
 		fprintf('saved new mep auto compute file\n %s\n with mep begin time = %f\n', save_file, mep_begin)
