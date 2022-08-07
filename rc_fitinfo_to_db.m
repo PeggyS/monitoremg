a2ad964f24785388b08582a100e42c89ec222e20
PeggyS <peggy.skelly@gmail.com>
2022-08-07 19:23:48 -0400

    Update rc_fitinfo_to_db.m
    add info from fitinfo.txt files to the mysql database

--- rc_fitinfo_to_db.m
+++ rc_fitinfo_to_db.m
@@ -1,8 +1,8 @@
-function m_h_data_to_db(analysisDir)
-% send the data in the m_max_h_reflex.csv files to the myomo.m_max_h_reflex table in the database
+function rc_fitinfo_to_db(analysisDir)
+% send the data in the fit_info.txt files to the tdcs_vgait. table in the database
 %
 % analysisDir is a string to the top level of the directory structure, e.g.
-% /Users/peggy/Documents/BrainLab/myomo_tbi/data/H_Reflex_M_Max/
+% /Users/peggy/Documents/BrainLab/tDCS Gait/analysis/tms/s2711tdvg/pre
 %	Default analysisDir is the current directory.
 
 if nargin < 1
@@ -10,7 +10,7 @@ if nargin < 1
 end
 
 % find fit info files
-file_list = regexpdir(analysisDir,'(m_max_h_reflex.csv)$');	
+file_list = regexpdir(analysisDir,'(.*_fit_info_.*\.txt)$');	
 if length(file_list) < 1
 	disp('found no _fit_info_*.txt files.')
 	return;
@@ -29,12 +29,13 @@ dbparams = get_db_login_params(db_str);
 
 try
 	conn = dbConnect(dbparams.dbname, dbparams.user, dbparams.password, dbparams.serveraddr);
-catch
+catch ME
+	disp(ME)
 	warning('could not connect to database')
 	return
 end
 % since this takes a while, display a waitbar
-hwb = waitbar(0, 'Sending H-Reflex Data to the Database');
+hwb = waitbar(0, 'Sending TMS Data to the Database');
 
 count = length(file_list);
 for i = 1:count
@@ -63,7 +64,7 @@ for i = 1:count
 	end
 	
 	% read in the subj's data
-	tbl = readtable();
+	info = read_fit_info(file_list{i});
 	
 	if isempty(session_str)		% no session found in the path
 		error('no session found in pathname %s', pathname)
@@ -83,14 +84,14 @@ for i = 1:count
 	% check to see if this is already in the database
 	db_data = conn.dbSearch('tms_rc_measures', {'norm_factor','last_update', 'id'}, 'subject', subj_str, ...
 		'session', session_str, 'side', side, 'muscle', muscle, ...
-		'mep_method', ds.mepMethod);
+		'mep_method', info.mepMethod);
 	
 	% if the info is not in the database or the file time is more recent than
 	% the database time for this info
 	% matching file info & db row for the norm factor == or ~= 1
 	add_or_update = 'add';
 	for r_cnt = 1:size(db_data,1)
-		if ds.norm_factor == 1 % entering not normed data
+		if info.norm_factor == 1 % entering not normed data
 			if db_data{r_cnt,1} == 1 % not norm database row
 				if file_date_num > datenum(db_data{r_cnt,2})
 					add_or_update = 'update';
@@ -115,11 +116,11 @@ for i = 1:count
 	if ~strcmp(add_or_update, 'skip')
 		disp(['processing ' file_list{i}])
 		
-		auc_vals_str = num2str(ds.aucMeanVals);
+		auc_vals_str = num2str(info.aucMeanVals);
 		assert(length(auc_vals_str)<=256, ...
 			'aucMeanVals converts to a string with %d chars -> longer than allowed by the database', ...
 			length(auc_vals_str))
-		stim_levels_str = num2str(ds.stimLevels);
+		stim_levels_str = num2str(info.stimLevels);
 		assert(length(stim_levels_str)<=256, ...
 			'stimLevels converts to a string with %d chars -> longer than allowed by the database', ...
 			length(stim_levels_str))
@@ -132,12 +133,14 @@ for i = 1:count
 						'norm_factor', 'mep_beg_t', 'mep_end_t', 'slope', 's50', ...
 						'mep_min', 'mep_max', 'slope_ci_1', 'slope_ci_2', 's50_ci_1', 's50_ci_2', ...
 						'mep_min_ci_1', 'mep_min_ci_2', 'mep_max_ci_1', 'mep_max_ci_2', 'r_sq', ...
-						'auc', 'auc_mean_values', 'auc_stim_levels'}, ...
-						{subj_str, session_str, side, muscle, ds.mepMethod, ...
-						ds.norm_factor, ds.mep_beg_t, ds.mep_end_t, ds.slope, ds.s50, ...
-						ds.mepMin, ds.mepMax, ds.slopeCi(1), ds.slopeCi(2), ds.s50Ci(1), ds.s50Ci(2), ...
-						ds.mepMinCi(1), ds.mepMinCi(2), ds.mepMaxCi(1), ds.mepMaxCi(2), ds.Rsq, ...
-						ds.auc, auc_vals_str, stim_levels_str});
+						'auc', 'auc_mean_values', 'auc_stim_levels', 'stimulator_mode', ...
+						'analyzed_by', 'analyzed_when'}, ...
+						{subj_str, session_str, side, muscle, info.mepMethod, ...
+						info.norm_factor, info.mep_beg_t, info.mep_end_t, info.slope, info.s50, ...
+						info.mepMin, info.mepMax, info.slopeCi(1), info.slopeCi(2), info.s50Ci(1), info.s50Ci(2), ...
+						info.mepMinCi(1), info.mepMinCi(2), info.mepMaxCi(1), info.mepMaxCi(2), info.Rsq, ...
+						info.auc, auc_vals_str, stim_levels_str, info.stimulator_mode, ...
+						info.analyzed_by, info.analyzed_when});
 				catch ME
 					match_cell = regexp(ME.message, '''.*''','match');
 					match_str = strrep(match_cell{1}, '''', '');
@@ -153,11 +156,13 @@ for i = 1:count
 					conn.dbUpdate('tms_rc_measures',{'norm_factor', 'mep_beg_t', 'mep_end_t', 'slope', 's50', ...
 						'mep_min', 'mep_max', 'slope_ci_1', 'slope_ci_2', 's50_ci_1', 's50_ci_2', ...
 						'mep_min_ci_1', 'mep_min_ci_2', 'mep_max_ci_1', 'mep_max_ci_2', 'r_sq', ...
-						'auc', 'auc_mean_values', 'auc_stim_levels'}, ...
-						{ds.norm_factor, ds.mep_beg_t, ds.mep_end_t, ds.slope, ds.s50, ...
-						ds.mepMin, ds.mepMax, ds.slopeCi(1), ds.slopeCi(2), ds.s50Ci(1), ds.s50Ci(2), ...
-						ds.mepMinCi(1), ds.mepMinCi(2), ds.mepMaxCi(1), ds.mepMaxCi(2), ds.Rsq, ...
-						ds.auc, auc_vals_str, stim_levels_str}, ...
+						'auc', 'auc_mean_values', 'auc_stim_levels', 'stimulator_mode', ...
+						'analyzed_by', 'analyzed_when'}, ...
+						{info.norm_factor, info.mep_beg_t, info.mep_end_t, info.slope, info.s50, ...
+						info.mepMin, info.mepMax, info.slopeCi(1), info.slopeCi(2), info.s50Ci(1), info.s50Ci(2), ...
+						info.mepMinCi(1), info.mepMinCi(2), info.mepMaxCi(1), info.mepMaxCi(2), info.Rsq, ...
+						info.auc, auc_vals_str, stim_levels_str, info.stimulator_mode, ...
+						info.analyzed_by, info.analyzed_when}, ...
 						'id', update_id);
 				catch ME
 					keyboard
@@ -173,29 +178,5 @@ conn.dbClose()
 % close the waitbar
 close(hwb);
 
-% ------------------------------------------------------------------------------
-function ds = readRcInfo(fname)
-ds = dataset();
-% read in the file as text
-txt = readtextfile(fname);
-if isempty(txt)
-	% nothing read in
-	disp('no text to parse')
-	return
+return
 end
-
-% separate each line at the colon ':'
-txt = cellfun(@(x)(regexp(x,':','split')), txt, 'uniformoutput', false);
-txt = cellfun(@strtrim,txt, 'uniformoutput', false);		% remove extra spaces
-
-% go through each line of text
-for kk = 1:length(txt)
-	% variable name is in the first cell, value is in the second
-	% normFactorStr and pctVsetting are strings, all others are 1 or 2 numbers
-	if regexp(txt{kk}{1}, 'normFactorStr|pctVsetting|mepMethod|coil')
-		ds = horzcat(ds, dataset({txt{kk}{2}, txt{kk}{1}}));
-	else
-		ds = horzcat(ds, dataset({str2num(txt{kk}{2}), txt{kk}{1}}));
-	end
-end
-
