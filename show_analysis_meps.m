e1142d4d2ec9eab4f686edfac42a9b65c9e5a6a7
PeggyS <peggy.skelly@gmail.com>
2022-12-29 15:43:22 -0500

    Update show_analysis_meps.m
    Add selecting the table rows used for ts, sici, & icf latencies.

--- show_analysis_meps.m
+++ show_analysis_meps.m
@@ -13,46 +13,77 @@ if isprop(app, 'mep_info')
 				app.mep_info.using_rc_or_sici_data ' data.'])
 			return
 		end
-		if app.CheckBoxSici.Value == 1 && ~contains(app.mep_info.using_rc_or_sici_data, 'sici')
-			disp(['Currently viewing SICI data. MEP times were computed using ' ...
-				app.mep_info.using_rc_or_sici_data ' data.'])
-			return
+% 		if app.CheckBoxSici.Value == 1 && ~contains(app.mep_info.using_rc_or_sici_data, 'sici')
+% 			disp(['Currently viewing SICI data. MEP times were computed using ' ...
+% 				app.mep_info.using_rc_or_sici_data ' data.'])
+% 			return
+% 		end
+
+		rows_used = [];
+		% get table rows used if rc analysis
+		if app.CheckBoxRc.Value == 1 && isfield(app.mep_info, 'epochs_used_for_latency')
+			if ~isempty(app.mep_info.epochs_used_for_latency)
+				rows_used = app.mep_info.epochs_used_for_latency;
+			end
 		end
-
-	if isfield(app.mep_info, 'epochs_used_for_latency')
-		if ~isempty(app.mep_info.epochs_used_for_latency)
-
-			% select the cells
+		% get tablw rows used if sici analysis
+		if app.CheckBoxSici.Value == 1 && isgraphics(app.sici_fig)
+			% selected table rows
+			% find currently selected cells
 			jUIScrollPane = findjobj(app.h_uitable);
 			jUITable = jUIScrollPane.getViewport.getView;
 			j_now_selected_rows = jUITable.getSelectedRows;
-
-			% toggle currently selected rows
-			for r_cnt = 1:length(j_now_selected_rows)
-				row = j_now_selected_rows(r_cnt);
-				col = 1;
-				% toggle the row
-				jUITable.changeSelection(row,col-1, true, false);
-			end
 			
-% 			% rows that are selected and should not be
-% 			tmp1 = setdiff(j_now_selected_rows+1, app.mep_info.epochs_used_for_latency);
-% 			
-% 			
-% 			% rows that are not selected but should be
-% 			tmp2 = setdiff(app.mep_info.epochs_used_for_latency, j_now_selected_rows+1);
-% 
-% 			rows2toggle = union(tmp1, tmp2);
-
-			% select the rows used
-			for r_cnt = 1:length(app.mep_info.epochs_used_for_latency)
-				row = app.mep_info.epochs_used_for_latency(r_cnt);
-				col = 1;
-				% toggle the row
-				jUITable.changeSelection(row-1,col-1, true, false);
+			% stim type col from the table
+			st_col = find(contains(app.h_uitable.ColumnName, 'Type'));
+			st_list = app.h_uitable.Data(j_now_selected_rows+1, st_col);
+			if length(st_list) > 1
+				st = unique(st_list);
+				if length(st) > 1
+					disp('more than 1 stim type chosen')
+					beep
+					return
+				end
+				st_var = lower(st{:});
+			else
+				st_var = lower(st_list{:});
+			end
+			if strcmp(st_var, 'test stim')
+				st_var = 'ts';
 			end
 
-		end % ~isempty
+			st_var_latency = [st_var '_latency'];
+
+			if isfield(app.sici_ui, st_var_latency) && isfield(app.sici_ui.(st_var_latency).UserData, 'epochs_used')
+				rows_used = app.sici_ui.(st_var_latency).UserData.epochs_used;
+			end
+		end
+
+		if isempty(rows_used)
+			return
+		end
+		
+		% find currently selected cells
+		jUIScrollPane = findjobj(app.h_uitable);
+		jUITable = jUIScrollPane.getViewport.getView;
+		j_now_selected_rows = jUITable.getSelectedRows;
+
+		% toggle currently selected rows
+		for r_cnt = 1:length(j_now_selected_rows)
+			row = j_now_selected_rows(r_cnt);
+			col = 1;
+			% toggle the row
+			jUITable.changeSelection(row,col-1, true, false);
+		end
+		
+		% select the rows used
+		for r_cnt = 1:length(rows_used)
+			row = rows_used(r_cnt);
+			col = 1;
+			% toggle the row
+			jUITable.changeSelection(row-1,col-1, true, false);
+		end
+
 	end % isfield
 end % isprop
 return
