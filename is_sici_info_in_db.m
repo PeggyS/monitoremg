b005ed695bad671615b40b3d32c94e077dbd9df5
PeggyS <peggy.skelly@gmail.com>
2023-03-07 13:43:44 -0500

    Finish adding sici to db stuff

--- is_sici_info_in_db.m
+++ is_sici_info_in_db.m
@@ -10,39 +10,84 @@ muscle = split_cell{2};
 
 db_tbl = get_sici_icf_data_from_db(app, subject, session, side, muscle);
 
-% FIXME
-if ~isempty(db_info)
-	app.InDatabaseCheckBox.Value = true;
-	app.dbLastupdatedEditField.Value = db_info.last_update;
+% 
+if ~isempty(db_tbl)
+	db_tbl = sortrows(db_tbl, 'stim_type');
+
+	app_tbl = app.UITable_Unique_Stims_sici.Data;
+	% change app table's variable names to match the database variable names
+	app_tbl.Properties.VariableNames = lower(app_tbl.Properties.VariableNames);
+	app_tbl.Properties.VariableNames = strrep(app_tbl.Properties.VariableNames, 'num_mep', 'num_meps');
+	app_tbl.Properties.VariableNames = strrep(app_tbl.Properties.VariableNames, 'mean_latency', 'mep_latency_mean');
+	app_tbl.Properties.VariableNames = strrep(app_tbl.Properties.VariableNames, 'mean_end', 'mep_end_time_mean');
+	app_tbl.Properties.VariableNames = strrep(app_tbl.Properties.VariableNames, 'mean_mep_ampl', 'mep_amplitude_mean');
+	app_tbl.Properties.VariableNames = strrep(app_tbl.Properties.VariableNames, 'num_comments', 'num_samples_with_comments');
+	app_tbl = sortrows(app_tbl, 'stim_type');
+
+	app.InDatabaseCheckBox_sici.Value = true;
+	app.dbLastupdatedEditField_sici.Value = db_tbl.last_update{1};
 	% does the database info match what's shown here?
-	match = false;
-	if db_info.effective_stimulator_output == app.UITable_Unique_Stims.UserData.effective_stimulator_output && ...
-		db_info.is_eff_so_max_stim == app.MEPmaxatMaxSOCheckBox.Value && ...
-		db_info.num_samples == app.UITable_Unique_Stims.UserData.num_samples && ...
-		db_info.num_meps == app.UITable_Unique_Stims.UserData.num_meps && ...
-		abs(db_info.mep_mean_latency - app.UITable_Unique_Stims.UserData.mean_latency) < 1e-6  && ...
-		abs(db_info.mep_mean_end_time - app.UITable_Unique_Stims.UserData.mean_end) < 1e-6 && ...
-		abs(db_info.mep_mean_amplitude - app.UITable_Unique_Stims.UserData.mep_max) < 1e-6 && ...
-		db_info.num_samples_with_comments == app.UITable_Unique_Stims.UserData.num_comments && ...
-		db_info.num_sd == app.NumSDEditField.Value && ...
-		abs(db_info.e_stim_m_max - app.EStimMmaxEditField.Value) < 1e-6 && ...
-		db_info.did_rc_plateau == app.RecruitmentCurvePlateauedCheckBox.Value && ...
-		contains(db_info.analyzed_by, app.AnalyzedbyEditField.Value) && ...
-		contains(db_info.analyzed_when, app.AnalysisdateEditField.Value) && ...
-		contains(db_info.verified_by, app.VerifiedbyEditField.Value) && ...
-		contains(db_info.verified_when, app.VerifydateEditField.Value) && ...
-		contains(db_info.comments, app.CommentsEditField.Value)  
+
+	% check main uitable data
+	if isequal(app_tbl(:, {'magstim_setting' 'bistim_setting' 'isi_ms' 'stim_type' 'num_samples' 'num_meps' 'num_samples_with_comments'}), ...
+				db_tbl(:, {'magstim_setting' 'bistim_setting' 'isi_ms' 'stim_type' 'num_samples' 'num_meps' 'num_samples_with_comments'}))
 		match = true;
+	else
+		match = false;
 	end
 	if match == true
-		app.DatabaseinfomatchesCheckBox.Value = true;
+		% check float/double vars that may not exactly match
+		var_list = {'mep_latency_mean' 'mep_end_time_mean' 'mep_amplitude_mean'};
+		for v_cnt = 1:length(var_list)
+			if max(abs(app_tbl.(var_list{v_cnt}) - db_tbl.(var_list{v_cnt}))) < 1e-3
+				match = true;
+			else
+				match = false;
+				break
+			end
+		end
+	end
+
+	if match == true
+		% check more_info table
+		more_tbl = sortrows(app.UITable_Unique_Stims_sici.UserData.more_info_tbl, 'stim_type');
+		more_tbl.Properties.VariableNames = strrep(more_tbl.Properties.VariableNames, 'sd_latency', 'mep_latency_sd');
+		more_tbl.Properties.VariableNames = strrep(more_tbl.Properties.VariableNames, 'sd_end', 'mep_end_time_sd');
+		more_tbl.Properties.VariableNames = strrep(more_tbl.Properties.VariableNames, 'sd_mep_ampl', 'mep_amplitude_sd');
+		more_tbl.Properties.VariableNames = strrep(more_tbl.Properties.VariableNames, 'ci1_mep_ampl', 'mep_ampl_98pct_ci_1');
+		more_tbl.Properties.VariableNames = strrep(more_tbl.Properties.VariableNames, 'ci2_mep_ampl', 'mep_ampl_98pct_ci_2');
+		var_list = {'mep_latency_sd' 'mep_end_time_sd' 'mep_amplitude_sd' 'mep_ampl_98pct_ci_1' 'mep_ampl_98pct_ci_2'};
+		for v_cnt = 1:length(var_list)
+			if max(abs(more_tbl.(var_list{v_cnt}) - db_tbl.(var_list{v_cnt}))) < 1e-3
+				match = true;
+			else
+				match = false;
+				break
+			end
+		end
+	end
+
+	% check app fields
+	if match && ...
+		db_tbl.num_sd(1) == app.NumSDEditField_sici.Value && ...
+		contains(db_tbl.analyzed_by{1}, app.AnalyzedbyEditField_sici.Value) && ...
+		contains(db_tbl.analyzed_when{1}, app.AnalysisdateEditField_sici.Value) && ...
+		contains(db_tbl.verified_by{1}, app.VerifiedbyEditField_sici.Value) && ...
+		contains(db_tbl.verified_when{1}, app.VerifydateEditField_sici.Value) && ...
+		contains(db_tbl.comments{1}, app.CommentsEditField_sici.Value)  
+		match = true;
 	else
-		app.DatabaseinfomatchesCheckBox.Value = false;
+		match = false;
+	end
+	if match == true
+		app.DatabaseinfomatchesCheckBox_sici.Value = true;
+	else
+		app.DatabaseinfomatchesCheckBox_sici.Value = false;
 	end
 else
-	app.InDatabaseCheckBox.Value = false;
-	app.dbLastupdatedEditField.Value = '';
-	app.DatabaseinfomatchesCheckBox.Value = false;
+	app.InDatabaseCheckBox_sici.Value = false;
+	app.dbLastupdatedEditField_sici.Value = '';
+	app.DatabaseinfomatchesCheckBox_sici.Value = false;
 end % info in the database
 
 return
