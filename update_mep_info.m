b8cf58818bf7a37a63714d2449747c56a5acf684
PeggyS <peggy.skelly@gmail.com>
2024-01-17 14:36:08 -0500

    add exchanging num manual adjust info with database

--- update_mep_info.m
+++ update_mep_info.m
@@ -46,14 +46,12 @@ for r_cnt = 1:height(unq_tbl)
 		unq_tbl.num_latency_manual_adjust(r_cnt) = sum(stim_tbl.latency_adjusted);
 	else
 		unq_tbl.num_latency_manual_adjust(r_cnt) = NaN;
-% 		unq_tbl = movevars(unq_tbl, 'num_latency_manual_adjust', 'After', 'mean_latency');
 	end
 	unq_tbl.mean_end(r_cnt) = mean(stim_tbl.MEP_end);
 	if any(contains(stim_tbl.Properties.VariableNames, 'end_adjusted'))
 		unq_tbl.num_end_manual_adjust(r_cnt) = sum(stim_tbl.end_adjusted);
 	else
 		unq_tbl.num_end_manual_adjust(r_cnt) = NaN;
-% 		unq_tbl = movevars(unq_tbl, 'num_end_manual_adjust', 'After', 'mean_end');
 	end
 	unq_tbl.mean_mep_ampl(r_cnt) = mean(stim_tbl.MEPAmpl_uVPp);
 	if isnumeric(stim_tbl.comments)
@@ -76,7 +74,9 @@ app.UITable_Unique_Stims.UserData.effective_stimulator_output = unq_tbl.Effectiv
 app.UITable_Unique_Stims.UserData.num_samples = unq_tbl.num_mep(max_row);
 app.UITable_Unique_Stims.UserData.num_meps = unq_tbl.num_mep(max_row);
 app.UITable_Unique_Stims.UserData.mean_latency = unq_tbl.mean_latency(max_row);
+app.UITable_Unique_Stims.UserData.num_latency_manual_adjust = unq_tbl.num_latency_manual_adjust(max_row);
 app.UITable_Unique_Stims.UserData.mean_end = unq_tbl.mean_end(max_row);
+app.UITable_Unique_Stims.UserData.num_end_manual_adjust = unq_tbl.num_end_manual_adjust(max_row);
 app.UITable_Unique_Stims.UserData.mep_max = max_val;
 app.UITable_Unique_Stims.UserData.num_comments = unq_tbl.num_comments(max_row);
 
