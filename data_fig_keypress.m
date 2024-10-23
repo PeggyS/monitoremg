1d3bdca0fa2513f0f5b4dd6fb64c40e9957b2bab
PeggyS <peggy.skelly@gmail.com>
2024-10-23 16:31:42 -0400

    enable arrow to move epoch number when emg save fig button is active

--- data_fig_keypress.m
+++ data_fig_keypress.m
@@ -1,20 +1,20 @@
 function data_fig_keypress(src, key_data, app)
-disp(key_data.Key)
+% disp(key_data.Key)
 % app.h_uitable.UserData.Selection
-% 
-% switch key_data.Key
-% 	case 'downarrow'
-% 		cell_select_data.Indices = app.h_uitable.UserData.Selection(end,:);
-% 		cell_select_data.Indices(1) = min([cell_select_data.Indices(1) + 1, size(app.h_uitable.Data,1)]);
-% 	case 'uparrow'
-% 		cell_select_data.Indices = app.h_uitable.UserData.Selection(1,:);
-% 		cell_select_data.Indices(1) = max([cell_select_data.Indices(1) - 1, 1]);
-% 	otherwise
-% 		cell_select_data.Indices = app.h_uitable.UserData.Selection;
-% end
-% 
-% cell_select_data.Source = src;
-% cell_select_data.EventName = 'CellSelection';
-% 
-% rc_dp_tbl_select_callback(app.h_uitable, cell_select_data, app)
+
+switch key_data.Key
+	case 'downarrow'
+		cell_select_data.Indices = app.h_uitable.UserData.Selection(end,:);
+		cell_select_data.Indices(1) = min([cell_select_data.Indices(1) + 1, size(app.h_uitable.Data,1)]);
+	case 'uparrow'
+		cell_select_data.Indices = app.h_uitable.UserData.Selection(1,:);
+		cell_select_data.Indices(1) = max([cell_select_data.Indices(1) - 1, 1]);
+	otherwise
+		cell_select_data.Indices = app.h_uitable.UserData.Selection;
+end
+
+cell_select_data.Source = src;
+cell_select_data.EventName = 'CellSelection';
+
+rc_dp_tbl_select_callback(app.h_uitable, cell_select_data, app)
 end
